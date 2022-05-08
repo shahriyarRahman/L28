0c796914c3b5adaf2ab8fe89fbf8d8633b879568
Zohaib Ashraf <zohaibashraf546@gmail.com>
2022-05-08 14:21:10 +0500

    repo init for L28

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+	content: ["./dist/**/*.{html,js}"],
+	theme: {
+		extend: {
+			colors: {
+				palette: {
+					1: "#08647C",
+					2: "#060192",
+					3: "#F7513E",
+					3: "#FCC455",
+					4: "#FFCC4D",
+					5: "#EE7584",
+					6: "#7070D4",
+				},
+			},
+		},
+	},
+	plugins: [],
+};
